ae5a3731619c3a81a94d5410531d92f250785573
yujinK <yujinkim.dev@gmail.com>
2019-07-23 23:42:28 +0900

    7. Add a Room database

--- app/src/main/java/com/example/roomwordsample/WordDao.kt
+++ app/src/main/java/com/example/roomwordsample/WordDao.kt
@@ -1,5 +1,6 @@
 package com.example.roomwordsample
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
@@ -7,11 +8,11 @@ import androidx.room.Query
 @Dao
 interface WordDao {
     @Query("SELECT * FROM word_table ORDER BY word ASC")
-    fun getAllWords(): List<Word>
+    fun getAllWords(): LiveData<List<Word>>
 
     @Insert
     suspend fun insert(word: Word)
-    
+
     @Query("DELETE FROM word_table")
     fun deleteAll()
 
